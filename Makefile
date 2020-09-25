c1f18c2dedfe5755fb68933b5294f90c358985fc
kvuj <timothee@fedora-linux-comp.(none)>
2020-09-24 22:28:35 -0400

    Cleanup

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-CC := g++
-CFLAGS := -O3 -std=c++17 -lSDL2
+CC 		:= g++
+CFLAGS		:= -O3 -std=c++17 -lSDL2
 
-CXX := $(CC)
-CXXFLAGS := $(CFLAGS)
+CXX 		:= $(CC)
+CXXFLAGS 	:= $(CFLAGS)
 
 wyvern: main.cpp
 	$(CC) -o output main.cpp bmsmap.hpp $(CFLAGS)
--- bmsmap.hpp
+++ bmsmap.hpp
@@ -1,25 +1,46 @@
 #ifndef BMSMAP
 #define BMSMAP
 
+#include <iostream>
+#include <fstream>
 #include <string>
 #include <filesystem>
 
 class bmsMap {
 private:
-	string *filename;
+    // FILEPATH    -> Folder name + bms file name.
+    // CURRENTLINE -> Current line scanned by the program.
+	std::string *folderName;
+	std::string *currentLine;
 
 public:
-	bmsMap(string *filename) {
+	bmsMap(std::string passedLocation) {
+	    // Removes the dot from ./<pathname>/name.bms
 
+	    if(passedLocation.at(0) == '.')
+            *folderName = "/bms" + passedLocation.substr(1, passedLocation.size());
+        else
+            *folderName = "/bms" + passedLocation;
 	}
 
 	~bmsMap() {
-	    delete filename;
+        delete folderName, currentLine;
+	
 	}
 
 	void loadNewMap() {
-        if(std::filesystem::exists(*filename) {
+	    // This checks for the bms file.
+	    // TODO Implement how to handle mutliple bms files in a dir.
+        if(std::filesystem::exists(*folderName)) {
+            std::ofstream myFile(*folderName);
 
+            while(!myFile.eof()) {
+
+
+            }
+        }
+        else {
+            std::cout << "Wrong folder you gave!";
         }
 	}
 };
